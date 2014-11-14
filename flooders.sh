5d74244ecaf7bccdfb931c58a001bea5f8bcbf81
Chrissy Weeks <fevrin@gmail.com>
2014-11-13 23:57:41 -0500

    flooders.sh + added check_file_for() to reduce redundant commands - removed redundant parts of grep one-liners

--- flooders.sh
+++ flooders.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-file=/tmp/2
+file=/tmp/1
 
 strip_file() {
    sed -i.bak -re 's;^time=.+msg="(.+)"$;\1;' $file
@@ -11,7 +11,7 @@ check_dest_ip() {
    echo "checking destination IP..."
 
    ip_regex="([0-9]{1,3}\.){3}[0-9]{1,3}"
-   local output=$(grep -Eoi --color=always "destination: $ip_regex" $file | sed 's;destination: ;;i' | sort | uniq -c | sort -n | tail -n5 | sed -r 's;^ +;;')
+   local output=$(check_file_for "$(grep -Eoi --color=always "destination: $ip_regex" $file | sed 's;destination: ;;i')")
 
    echo "$output" | while read; do
       local count=$(echo $REPLY | grep -Eo '^[0-9]+')
@@ -31,9 +31,14 @@ get-orgname-from-ip() {
    whois $ip | sed -rne 's;^(OrgName|org-name|descr):\s+(.+)$;\2;p' | sort -u | head -n1
 }
 
+check_file_for() {
+   local specifics="$1"
+   [[ -n $specifics ]] && echo "$specifics" | sort | uniq -c | sort -n | tail -n5 | sed -r 's;^ +;;'
+}
+
 check_dest_port() {
    echo "checking destination port..."
-   grep -Eoi --color=always 'destination port: [0-9]+' $file | sed 's;destination port: ;;i' | sort | uniq -c | sort -n | tail -n5 | sed -r 's;^ +;;'
+   check_file_for "$(grep -Eoi --color=always 'destination port: [0-9]+' $file | sed 's;destination port: ;;i')"
 }
 
 check_syn() {
@@ -48,7 +53,7 @@ check_syn() {
 
 check_udp() {
    echo "checking udp..."
-   grep -Eoi --color=always 'user datagram protocol[^"\n]*' $file | sort | uniq -c | sort -n | sed -r 's;^ +;;'
+   check_file_for "$(grep -Eoi --color=always 'user datagram protocol[^"\n]*' $file)"
 }
 
 total_packets() {
@@ -57,7 +62,7 @@ total_packets() {
 
 check_dns_amp() {
    echo "checking DNS amplification..."
-   grep -Eoi --color=always 'Recursion desired: Do query recursively' $file | sort | uniq -c | sort -n | sed -r 's;^ +;;'
+   check_file_for "$(grep -Eoi --color=always 'Recursion desired: Do query recursively' $file)"
 }
 
 strip_file
